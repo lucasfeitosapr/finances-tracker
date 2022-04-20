5acf48bb3bfcff04fa405ad1d768109c201080a7
Lucas Feitosa <lucasfeitosapr@gmail.com>
2022-04-19 21:53:36 -0300

    Impl repository

--- finances-tracker/src/main/java/br/com/gebotech/financestracker/repository/RoleRepository.java
+++ finances-tracker/src/main/java/br/com/gebotech/financestracker/repository/RoleRepository.java
@@ -1,4 +1,15 @@
 package br.com.gebotech.financestracker.repository;
 
-public interface RoleRepository {
+import br.com.gebotech.financestracker.models.Role;
+import br.com.gebotech.financestracker.models.enums.ERole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role>findByName(ERole name);
+
 }
--- finances-tracker/src/main/java/br/com/gebotech/financestracker/repository/UserRepository.java
+++ finances-tracker/src/main/java/br/com/gebotech/financestracker/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package br.com.gebotech.financestracker.repository;
 
-public interface UserRepository {
+import br.com.gebotech.financestracker.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
