0e5fb56f64cb31020fd2438c4f0539d03e47244f
Lucas Feitosa <lucasfeitosapr@gmail.com>
2022-04-19 21:45:43 -0300

    Impl models

--- finances-tracker/src/main/java/br/com/gebotech/financestracker/models/Role.java
+++ finances-tracker/src/main/java/br/com/gebotech/financestracker/models/Role.java
@@ -1,4 +1,37 @@
 package br.com.gebotech.financestracker.models;
 
+import br.com.gebotech.financestracker.models.enums.ERole;
+
+import javax.persistence.*;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
 }
--- finances-tracker/src/main/java/br/com/gebotech/financestracker/models/enums/ERole.java
+++ finances-tracker/src/main/java/br/com/gebotech/financestracker/models/enums/ERole.java
@@ -1,4 +1,7 @@
 package br.com.gebotech.financestracker.models.enums;
 
 public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
 }
